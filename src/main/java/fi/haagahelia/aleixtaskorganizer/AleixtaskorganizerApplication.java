7dbc197081605572487f29000ac9c0633c1e872b
Juslin Jukka <jusju@haaga-helia.fi>
2022-03-31 09:53:37 +0300

    first commit

---
+++ src/main/java/fi/haagahelia/aleixtaskorganizer/AleixtaskorganizerApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.aleixtaskorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AleixtaskorganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AleixtaskorganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/haagahelia/aleixtaskorganizer/AleixtaskorganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.aleixtaskorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AleixtaskorganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
